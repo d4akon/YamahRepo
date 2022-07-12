8f2bbad60e9494915ee5615c0420531e24d49a95
Jakub Machnica <85107158+EZiK4444@users.noreply.github.com>
2022-07-12 23:43:28 +0200

    Added menu window of app
    Added menu window in xaml, class and viewmodel

---
+++ ViewModels/MenuViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yamah.Core;
+using Yamah.Models;
+
+namespace Yamah.ViewModels
+{
+    internal class MenuViewModel : ObservableObject
+    {
+        public User? User { get; set; } = LoginViewModel.User;
+        /* Comands */
+        public RelayCommand MoveWindowCommand { get; set; }
+
+        public RelayCommand ShutdownWindowCommand { get; set; }
+
+        public RelayCommand MinimizeWindowCommand { get; set; }
+
+        public RelayCommand MaximizeWindowCommand { get; set; }
+
+        private int _currentView;
+
+        public int CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MenuViewModel()
+        {
+            Application.Current.MainWindow.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+
+            MoveWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.DragMove(); });
+
+            ShutdownWindowCommand = new RelayCommand(o => { Application.Current.Shutdown(); });
+
+            MinimizeWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.WindowState = WindowState.Minimized; });
+
+            MaximizeWindowCommand = new RelayCommand(o =>
+            {
+                if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                else
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            });
+        }
+    }
+}
---
+++ Views/MenuWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Yamah.Views;
+using Yamah.ViewModels;
+
+namespace Yamah.Views
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        protected const string connectionString = @"Data Source=localhost\SQLCOURSE2019;Initial Catalog=Yamah;Integrated Security=True";
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void UsersButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new UsersPage();
+        }
+
+        private void TasksButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new TasksPage();
+        }
+
+        private void LogoutButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Visibility = Visibility.Visible;
+            Window win = (Window)this.Parent;
+            this.Close();
+        }
+
+        private void AddNewTaskButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var users = UsersViewModel.GetAllUsers(connectionString);
+            List<string> usernames = new List<string>();
+            foreach (var u in users)
+            {
+                usernames.Add(u.Username);
+            }
+
+            AddNewTaskWindow addNewTaskWindow = new AddNewTaskWindow();
+
+            addNewTaskWindow.Visibility = Visibility.Visible;
+            addNewTaskWindow.AssignedUserBox.ItemsSource = usernames;
+        }
+    }
+}
